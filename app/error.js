632982854df73d2c1cace75487926fb11aaf8b1b
FaridMo99 <farid-ahmad@hotmail.de>
2025-06-18 20:17:46 +0200

    added api route

--- app/error.js
+++ app/error.js
@@ -3,7 +3,7 @@ import React from "react";
 import Link from "next/link";
 import { AlertTriangle } from "lucide-react";
 
-function Error({reset}) {
+function Error({ reset }) {
   return (
     <main className="flex flex-col items-center justify-center flex-grow p-8 text-center">
       <AlertTriangle size={64} className="text-red-500 mb-6 animate-pulse" />
@@ -11,15 +11,18 @@ function Error({reset}) {
         Oops! Something went wrong.
       </h1>
       <div className="flex justify-between items-center">
-      <button className="text-white px-6 py-3 rounded-md bg-secondary border border-secondary-light" onClick={reset}>
-        Try Again
+        <button
+          className="text-white px-6 py-3 rounded-md bg-secondary border border-secondary-light"
+          onClick={reset}
+        >
+          Try Again
         </button>
-      <Link
-        href="/"
-        className=" text-white px-6 py-3 ml-6 rounded-md bg-secondary border border-secondary-light"
-      >
-        Go Home
-      </Link>
+        <Link
+          href="/"
+          className=" text-white px-6 py-3 ml-6 rounded-md bg-secondary border border-secondary-light"
+        >
+          Go Home
+        </Link>
       </div>
     </main>
   );
